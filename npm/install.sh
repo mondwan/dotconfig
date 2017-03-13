13116b098d347f3ae4c47d26401ad54a0fcd1048
Mond Wan <mondwan.1015@gmail.com>
2017-03-13 11:05:22 +0800

    Update version of NPM packages
    * Decouple the hard coded strings from install.sh to pacakges.txt

--- npm/install.sh
+++ npm/install.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-pkgs=" eslint"
-pkgs="${pkgs} babel-eslint"
-pkgs="${pkgs} eslint-plugin-jsx-a11y@^2.2.3"
-pkgs="${pkgs} eslint-plugin-import@^2.1.0"
-pkgs="${pkgs} eslint-plugin-react"
-pkgs="${pkgs} eslint-config-airbnb"
-pkgs="${pkgs} eslint-config-eslint"
-pkgs="${pkgs} grunt-cli"
-pkgs="${pkgs} bower"
-pkgs="${pkgs} phantomjs-prebuilt"
+BASEDIR=$(dirname "$0")
 
-for pkg in ${pkgs}; do
-    echo "npm install -g ${pkg}"
-    npm install -g ${pkg} || exit 1
-done
+paste -d' ' -s "$BASEDIR/packages.txt" | \
+    xargs -I{} sh -c "echo npm install -g {}; npm install -g {} || exit 255"
