ec37ace7542fbc0c454ca9ece5a6b746702daea6
Mond Wan <mondwan.1015@gmail.com>
2017-03-17 20:55:14 +0800

    Split xargs into 2 commands
    * In OSX, xargs stops replacing (-I) if length of command is already
      longer than 255 characters

--- npm/install.sh
+++ npm/install.sh
@@ -2,5 +2,9 @@
 
 BASEDIR=$(dirname "$0")
 
+# Split xargs into two different commands as it has a length limitation when
+# replacing command argument
 paste -d' ' -s "$BASEDIR/packages.txt" | \
-    xargs -I{} sh -c "echo npm install -g {}; npm install -g {} || exit 255"
+    xargs -I{} sh -c "echo npm install -g {}"
+paste -d' ' -s "$BASEDIR/packages.txt" | \
+    xargs -I{} sh -c "npm install -g {} || exit 255"
