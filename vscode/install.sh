4a1449f23c6d29f1f2d46bc467420f058ed84a81
Mond Wan <mondwan.1015@gmail.com>
2022-05-29 19:32:28 +0100

    Fix running issues in mac os

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,9 +2,11 @@
 
 BASEDIR=$(dirname "$0")
 
+echo "$BASEDIR"
+
 # Split xargs into two different commands as it has a length limitation when
 # replacing command argument
 paste -d' ' -s "$BASEDIR/packages.txt" | \
-    xargs -I{} sh -c "echo code --install-extension {}"
+    xargs -n1 -I{} sh -c "echo code --install-extension {}"
 paste -d' ' -s "$BASEDIR/packages.txt" | \
-    xargs -I{} sh -c "code --install-extension {} || exit 255"
+    xargs -n1 -I{} sh -c "code --install-extension {} || exit 255"
